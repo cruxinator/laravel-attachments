7f3d3848e691739f1b70ea3285410394b4ebec79
Alex Goodwin <cyberia.resurrection@gmail.com>
2022-09-07 16:24:05 +1000

    Wring out migrateAttachments command

--- tests/Console/MigrateAttachmentsTest.php
+++ tests/Console/MigrateAttachmentsTest.php
@@ -2,11 +2,14 @@
 
 namespace Cruxinator\Attachments\Tests\Console;
 
+use Cruxinator\Attachments\Models\Attachment;
 use Cruxinator\Attachments\Tests\TestCase;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Storage;
+use Mockery as m;
 
 class MigrateAttachmentsTest extends TestCase
 {
@@ -56,5 +59,49 @@ class MigrateAttachmentsTest extends TestCase
         Storage::shouldReceive('disk->has')->andThrow(\Exception::class);
 
         $res = Artisan::call('attachments:migrate', ['from' => $from, 'to' => $to]);
+    }
+
+    public function testMigrateTwoAttachmentsBothExistingFiles()
+    {
+        $from = 'local';
+        $to = 's3';
+
+        $att1 = new Attachment();
+        $att1->disk = 'local';
+        $att1->filepath = '';
+        $att1->filename = '';
+        $att1->filetype = '';
+        $att1->filesize = 0;
+        $att1->group = 'aybabtu';
+        $att1->save();
+
+        $att2 = new Attachment();
+        $att2->disk = 'local';
+        $att2->filepath = '';
+        $att2->filename = '';
+        $att2->filetype = '';
+        $att2->filesize = 0;
+        $att2->group = 'aybabtu';
+        $att2->save();
+        
+        $local = m::mock(FilesystemAdapter::class)->makePartial();
+        $local->expects('exists')->andReturns(true)->twice();
+        $local->allows('has')->andReturns(true);
+        $local->allows('get')->andReturns('HAMMERTIME');
+        
+        $s3 = m::mock(FilesystemAdapter::class)->makePartial();
+        $s3->allows('has')->andReturns(true);
+        $s3->allows('put')->andReturns(true);
+
+        Storage::shouldReceive('disk')->withArgs(['local'])->andReturn($local);
+        Storage::shouldReceive('disk')->withArgs(['s3'])->andReturn($s3);
+
+        $res = $this->artisan('attachments:migrate', ['from' => $from, 'to' => $to])->run();
+        $this->assertEquals(0, $res);
+        
+        $att1->refresh();
+        $this->assertEquals('s3', $att1->disk, 'First attachment disk not updated');
+        $att2->refresh();
+        $this->assertEquals('s3', $att2->disk, 'Second attachment disk not updated');
     }
 }
